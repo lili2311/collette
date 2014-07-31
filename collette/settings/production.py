55c6c9659e4558988d64ebd1c22f3ea762f0524a
Liliana Kastilio <kastilio.liliana@gmail.com>
2014-07-31 21:17:19 +0100

    Production database settings added

--- collette/settings/production.py
+++ collette/settings/production.py
@@ -1,10 +1,16 @@
 from collette.settings import *
+# Parse database configuration from $DATABASE_URL
+import dj_database_url
+
+DATABASES['default'] =  dj_database_url.config()
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 # Allow all host headers
 ALLOWED_HOSTS = ['agile-sierra-7809.herokuapp.com']
+
+
 
 LOGGING = {
     'version': 1,
